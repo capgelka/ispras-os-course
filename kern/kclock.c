6f767d45338770ab8a5fdffd5e9af82adfc3a681
capgelka <ldyach@yandex.ru>
2018-04-16 23:56:08 +0300

    init clock and set cli in entry.S

--- kern/kclock.c
+++ kern/kclock.c
@@ -2,12 +2,19 @@
 
 #include <inc/x86.h>
 #include <kern/kclock.h>
+#include <inc/stdio.h>
 
 void
 rtc_init(void)
 {
 	nmi_disable();
 	// LAB 4: your code here
+    outb(IO_RTC_CMND, RTC_BREG);
+    uint8_t curr_value = inb(IO_RTC_DATA);
+    cprintf("===== %x\n", curr_value);
+    curr_value |= 0x00000010;
+    outb(IO_RTC_DATA, curr_value);
+    cprintf("===== %x\n", curr_value);
 
 	nmi_enable();
 }
@@ -17,7 +24,7 @@ rtc_check_status(void)
 {
 	uint8_t status = 0;
 	// LAB 4: your code here
-
+    outb(IO_RTC_CMND, RTC_CREG);
+    status = inb(IO_RTC_DATA);
 	return status;
 }
-
