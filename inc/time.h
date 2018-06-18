05ed39f68fcdcb51dde0d085b510d9a41d29c8f1
capgelka <ldyach@yandex.ru>
2018-06-18 04:38:35 +0300

    basic definitions

--- inc/time.h
+++ inc/time.h
@@ -4,6 +4,8 @@
 #ifndef JOS_INC_TIME_H
 #define JOS_INC_TIME_H
 
+typedef uint32_t time_t;
+
 struct tm
 {
     int tm_sec;                   /* Seconds.     [0-60] */
@@ -13,6 +15,15 @@ struct tm
     int tm_mon;                   /* Month.       [0-11] */
     int tm_year;                  /* Year - 1900.  */
 };
+
+
+struct timespec
+{
+    time_t  tv_sec;
+    long    tv_nsec;
+};
+
+
 
 bool is_leap_year(int year)
 {
@@ -100,4 +111,39 @@ void snprint_datetime(char *buf, int size, struct tm *tm)
           tm->tm_hour, tm->tm_min, tm->tm_sec);
 }
 
+
+
+
+#define CLOCK_MONOTONIC 1
+#define CLOCK_REALTIME 2
+#define CLOCK_PROCESS_CPUTIME_ID 3
+
+typedef uint8_t clockid_t;
+
+int clock_getres(clockid_t clock_id, struct timespec *res);
+int clock_gettime(clockid_t clock_id, struct timespec *tp);
+int clock_settime(clockid_t clock_id, const struct timespec *tp);
+
 #endif
+// В рамках решения индивидуального задания от Вас требуется реализовать
+// поддержку функций работы с часами [1,2]
+// int clock_getres(clockid_t clock_id, struct timespec *res);
+// int clock_gettime(clockid_t clock_id, struct timespec *tp);
+// int clock_settime(clockid_t clock_id, const struct timespec *tp);
+// int clock_nanosleep(clockid_t clock_id, int flags, const struct timespec
+// *rqtp, struct timespec *rmtp);
+
+// для часов [3]
+// CLOCK_MONOTONIC
+// CLOCK_REALTIME
+// CLOCK_PROCESS_CPUTIME_ID
+
+// [1]
+// http://pubs.opengroup.org/onlinepubs/9699919799/functions/clock_settime.html
+// [2]
+// http://pubs.opengroup.org/onlinepubs/9699919799/functions/clock_nanosleep.html
+// [3] http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/time.h.html
+
+
+// Работу над индивидуальным заданием необходимо вести в отдельной ветке itask.
+// Всё, что необходимо для выполнения тестов, также надо выложить в эту ветку.
--- kern/kclock.h
+++ kern/kclock.h
@@ -2,6 +2,7 @@
 
 #ifndef JOS_KERN_KCLOCK_H
 #define JOS_KERN_KCLOCK_H
+
 #ifndef JOS_KERNEL
 # error "This is a JOS kernel header; user programs should not #include it"
 #endif
